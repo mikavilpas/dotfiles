153caa8b36d07f575c31c3cef3d5b9e762761bc9
Mika Vilpas <mika.vilpas@barona.fi>
2024-01-10 08:27:29 +0200

    feat: leap has nicer colors

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -8,5 +8,27 @@ return {
     end
 
     -- don't add the default keybindings
+    --
+    -- Improve the colors
+    vim.api.nvim_set_hl(0, "LeapBackdrop", {})
+    vim.api.nvim_set_hl(0, "LeapMatch", {
+      -- For light themes, set to 'black' or similar.
+      fg = "white",
+      bold = true,
+      nocombine = true,
+    })
+
+    vim.api.nvim_set_hl(0, "LeapLabelPrimary", {
+      fg = "pink",
+      bold = true,
+      nocombine = true,
+    })
+    vim.api.nvim_set_hl(0, "LeapLabelSecondary", {
+      fg = "blue",
+      bold = true,
+      nocombine = true,
+    })
+    -- Try it without this setting first, you might find you don't even miss it.
+    require("leap").opts.highlight_unlabeled_phase_one_targets = true
   end,
 }
