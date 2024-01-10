3a684a9db4acff78d67e64831f58c207b65cc371
Mika Vilpas <mika.vilpas@barona.fi>
2024-01-10 21:09:00 +0200

    feat: open lazygit history for the current file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,3 +16,11 @@ vim.api.nvim_create_autocmd("BufEnter", {
   end,
   desc = "Disable New Line Comment",
 })
+
+local Util = require("lazyvim.util")
+
+-- open lazygit history for the current file
+vim.keymap.set("n", "<leader>gl", function()
+  local path = vim.api.nvim_buf_get_name(0)
+  Util.terminal({ "lazygit", "--filter", path }, { cwd = Util.root(), esc_esc = false, ctrl_hjkl = false })
+end, { desc = "Lazygit (root dir)" })
