37c0708b78a23f7fbdf29bb54c81cc1ddd09e46b
Mika Vilpas <mika.vilpas@barona.fi>
2024-01-11 21:43:39 +0200

    feat: jump to character with spc spc

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -10,6 +10,17 @@ return {
         local hop = require("hop")
         hop.hint_lines_skip_whitespace({})
       end,
+      desc = "Hop to line",
+    },
+
+    {
+      "<leader><leader>",
+      mode = { "n", "x", "o" },
+      function()
+        local hop = require("hop")
+        hop.hint_char1({})
+      end,
+      desc = "Hop to given character",
     },
   },
 }
