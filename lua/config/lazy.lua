d5b43913a00da4f7f8fff813f9d54eec8e1e5823
Mika Vilpas <mika.vilpas@barona.fi>
2024-01-14 20:41:25 +0200

    feat: enable prettier plugin from extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.editor.navic" },
     { import = "lazyvim.plugins.extras.util.project" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
 
     -- import/override with your plugins
     { import = "plugins" },
