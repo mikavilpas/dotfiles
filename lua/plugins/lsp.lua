fde398e281b7d77a3ac8ac55fb7f4e9d36ff065c
Mika Vilpas <mika.vilpas@barona.fi>
2024-01-15 15:15:38 +0200

    feat: enable inlay hints for typescript

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,49 @@
+-- https://www.lazyvim.org/plugins/lsp
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = { "jose-elias-alvarez/typescript.nvim" },
+
+  opts = {
+
+    servers = {
+      tsserver = {
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
+              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+          javascript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
+              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              includeInlayVariableTypeHints = true,
+
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+        },
+      },
+    },
+    inlay_hints = {
+      enabled = true,
+    },
+    setup = {
+      tsserver = function(_, opts)
+        require("typescript").setup({ server = opts })
+        return true
+      end,
+    },
+  },
+}
