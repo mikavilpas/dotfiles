31eabe959766130af3c8d981c389e9a9d7260c74
Mika Vilpas <mika.vilpas@barona.fi>
2024-01-15 15:17:51 +0200

    feat: telescope matching is top down and satisfying

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,4 +5,13 @@ return {
     -- disable the keymap to search files
     { "<leader><leader>", false },
   },
+
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+    },
+  },
 }
