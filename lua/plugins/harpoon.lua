474f56d54d1b1961799f32801f1427d2aa05fc8b
Mika Vilpas <mika.vilpas@barona.fi>
2024-01-16 11:07:12 +0200

    feat: add harpoon package

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,43 @@
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+
+  config = function()
+    require("harpoon"):setup({})
+  end,
+
+  keys = {
+    {
+      "<leader>he",
+      function()
+        local harpoon = require("harpoon")
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = "Open harpoon window",
+    },
+    {
+      "<leader>ha",
+      function()
+        require("harpoon"):list():append()
+      end,
+      desc = "Harpoon add file",
+    },
+    {
+      "ö",
+      function()
+        require("harpoon"):list():prev()
+      end,
+      desc = "Harpoon previous file",
+    },
+    {
+      "ä",
+      function()
+        require("harpoon"):list():next()
+      end,
+      desc = "Harpoon next file",
+    },
+  },
+}
