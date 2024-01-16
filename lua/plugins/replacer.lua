203b9d34697aad1fafc7792118aa9bc71139ba8f
Mika Vilpas <mika.vilpas@barona.fi>
2024-01-16 11:06:54 +0200

    feat!: replacer is used with <leader>H instead of <leader>h

--- lua/plugins/replacer.lua
+++ lua/plugins/replacer.lua
@@ -4,12 +4,12 @@ return {
   -- usage:
   -- - run telescope to search for a string somehow
   -- - <c-q> to save the results to a quickfix list
-  -- - <leader>h to run replacer.nvim
+  -- - run replacer.nvim with the keybinding below
   -- - <c-s> to save the changes
   opts = { rename_files = false },
   keys = {
     {
-      "<leader>h",
+      "<leader>H",
       function()
         require("replacer").run()
       end,
