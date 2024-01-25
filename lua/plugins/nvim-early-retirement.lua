701edb0e6c0176a8a4193d16fef24462522e0254
Mika Vilpas <mika.vilpas@barona.fi>
2024-01-25 16:32:26 +0200

    feat: nvim-early-retiriment closes unused buffers after 5 mins (was 20)

--- lua/plugins/nvim-early-retirement.lua
+++ lua/plugins/nvim-early-retirement.lua
@@ -1,6 +1,9 @@
 ---@type LazySpec
 return {
+  -- https://github.com/chrisgrieser/nvim-early-retirement/blob/main/lua/early-retirement.lua
   "chrisgrieser/nvim-early-retirement",
-  config = true,
+  config = {
+    retirementAgeMins = 5,
+  },
   event = "VeryLazy",
 }
