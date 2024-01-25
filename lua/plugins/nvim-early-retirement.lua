864fcec73a6fa7a69bcd41dcdea6a770522416ef
Mika Vilpas <mika.vilpas@barona.fi>
2024-01-25 16:41:18 +0200

    fix: nvim-early-retirement invalid options

--- lua/plugins/nvim-early-retirement.lua
+++ lua/plugins/nvim-early-retirement.lua
@@ -2,7 +2,7 @@
 return {
   -- https://github.com/chrisgrieser/nvim-early-retirement/blob/main/lua/early-retirement.lua
   "chrisgrieser/nvim-early-retirement",
-  config = {
+  opts = {
     retirementAgeMins = 5,
   },
   event = "VeryLazy",
