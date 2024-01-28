3fab594f7f6591c89cabea8ba410a7c26b982c8f
Mika Vilpas <mika.vilpas@barona.fi>
2024-01-28 19:28:25 +0200

    feat: staged hunks are highlighted on the left side of the code
    Previously, only unstaged changes were highlighted

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,9 @@
+---@type LazySpec
+return {
+  "lewis6991/gitsigns.nvim",
+  ---@type Gitsigns.Config
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    _signs_staged_enable = true,
+  },
+}
