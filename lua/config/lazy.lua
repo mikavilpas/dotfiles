8af0aebb9f17f0d6bddd5f32d3f1b56726441f96
Mika Vilpas <mika.vilpas@barona.fi>
2024-01-29 09:31:13 +0200

    feat: neovide support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -53,3 +53,21 @@ require("lazy").setup({
     },
   },
 })
+
+if vim.g.neovide then
+  vim.g.neovide_scroll_animation_length = 0.1
+  -- required to disable this to enable using {"!@$ keys on osx (probably has
+  -- something to do with alt gr / option)
+  vim.g.neovide_input_macos_alt_is_meta = false
+
+  -- enable some shortcuts with with cmd+v
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+
+  vim.g.neovide_input_ime = true
+
+  -- vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  -- vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  -- vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  -- vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+end
