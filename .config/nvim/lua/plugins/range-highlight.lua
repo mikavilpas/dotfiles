3b0bf597d754749ebf5232d1d3e94ae609f11fc7
Mika Vilpas <mika.vilpas@gmail.com>
2024-02-05 08:26:48 +0200

    feat(nvim): add range-highlight

---
+++ .config/nvim/lua/plugins/range-highlight.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+return {
+  "winston0410/range-highlight.nvim",
+  -- https://github.com/winston0410/range-highlight.nvim
+  enabled = true,
+  event = { "CmdlineEnter" },
+  dependencies = { "winston0410/cmd-parser.nvim" },
+  config = function()
+    require("range-highlight").setup({})
+  end,
+}
