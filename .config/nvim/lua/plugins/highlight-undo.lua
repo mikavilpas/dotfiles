8764e9054ab1e93aac497db571caa74ca1d5d6d0
Mika Vilpas <mika.vilpas@gmail.com>
2024-02-05 08:27:08 +0200

    feat(nvim): add highlight-undo

---
+++ .config/nvim/lua/plugins/highlight-undo.lua
@@ -0,0 +1,8 @@
+---@type LazySpec
+return {
+  "tzachar/highlight-undo.nvim",
+  -- Highlight changed text after Undo / Redo operations. Purely lua / nvim api
+  -- implementation, no external dependencies needed.
+  --
+  -- https://github.com/tzachar/highlight-undo.nvim
+}
