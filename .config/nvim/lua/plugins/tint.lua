3b1a6121d50fe506ade7973958cdec5255e83399
Mika Vilpas <mika.vilpas@gmail.com>
2024-02-07 19:24:23 +0200

    refactor(nvim): add explanation for levouh/tint.nvim plugin

--- .config/nvim/lua/plugins/tint.lua
+++ .config/nvim/lua/plugins/tint.lua
@@ -1,5 +1,6 @@
 ---@type LazySpec
 return {
+  -- Dim inactive windows in Neovim using window-local highlight namespaces.
   -- https://github.com/levouh/tint.nvim
   "levouh/tint.nvim",
   event = "VeryLazy",
