82d7fa14c92744d19c337f672c712ea2e096ac64
Mika Vilpas <mika.vilpas@gmail.com>
2024-02-23 20:11:12 +0200

    feat(nvim): embedded lazygit can open files in in host tabs

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -16,7 +16,14 @@ vim.api.nvim_create_autocmd("BufEnter", {
 -- open lazygit history for the current file
 vim.keymap.set("n", "<leader>gl", function()
   local path = vim.api.nvim_buf_get_name(0)
-  Util.terminal({ "lazygit", "--filter", path }, { cwd = Util.root(), esc_esc = false, ctrl_hjkl = false })
+  Util.terminal({ "lazygit", "--filter", path }, {
+    cwd = Util.root(),
+    esc_esc = false,
+    ctrl_hjkl = false,
+    env = {
+      EDITOR = [[nvim --server "$NVIM" --remote-tab ]],
+    },
+  })
 end, { desc = "lazygit file commits" })
 
 -- disable esc j and esc k moving lines accidentally
