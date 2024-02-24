fddff85644cb4a14d3e247f5dba9c54da24f12cb
Mika Vilpas <mika.vilpas@gmail.com>
2024-02-24 12:56:48 +0200

    feat(nvim): lsp inlay hints are not as distracting

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -13,8 +13,56 @@ return {
       -- colorscheme = "solarized-osaka",
       -- colorscheme = "catppuccin-latte",
       -- colorscheme = "catppuccin-frappe",
-      colorscheme = "catppuccin-macchiato",
+      colorscheme = "catppuccin",
       -- colorscheme = "catppuccin-mocha",
+    },
+  },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+
+    ---@type CatppuccinOptions
+    opts = {
+      -- https://github.com/catppuccin/nvim?tab=readme-ov-file#configuration
+      flavour = "macchiato",
+
+      dim_inactive = { enabled = true, percentage = 0.9 },
+
+      highlight_overrides = {
+        macchiato = function(colors)
+          -- macchiato colors:
+          -- rosewater = "#f4dbd6",
+          -- flamingo = "#f0c6c6",
+          -- pink = "#f5bde6",
+          -- mauve = "#c6a0f6",
+          -- red = "#ed8796",
+          -- maroon = "#ee99a0",
+          -- peach = "#f5a97f",
+          -- yellow = "#eed49f",
+          -- green = "#a6da95",
+          -- teal = "#8bd5ca",
+          -- sky = "#91d7e3",
+          -- sapphire = "#7dc4e4",
+          -- blue = "#8aadf4",
+          -- lavender = "#b7bdf8",
+          -- text = "#cad3f5",
+          -- subtext1 = "#b8c0e0",
+          -- subtext0 = "#a5adcb",
+          -- overlay2 = "#939ab7",
+          -- overlay1 = "#8087a2",
+          -- overlay0 = "#6e738d",
+          -- surface2 = "#5b6078",
+          -- surface1 = "#494d64",
+          -- surface0 = "#363a4f",
+          -- base = "#24273a",
+          -- mantle = "#1e2030",
+          -- crust = "#181926",
+          return {
+            LspInlayHint = { fg = colors.surface0, bg = colors.none },
+          }
+        end,
+      },
     },
   },
 }
--- .config/nvim/lua/plugins/tint.lua
+++ .config/nvim/lua/plugins/tint.lua
@@ -6,6 +6,21 @@ return {
   event = "VeryLazy",
 
   config = function(_)
-    require("tint").setup({})
+    require("tint").setup({
+      tint_background_colors = true,
+      transforms = {
+        function(r, g, b, hl_group_info)
+          -- I have set catppuccin tod dim the background colors of inactive
+          -- windows. That causes the text for LspInlayHints to become much
+          -- more readable. This makes the text to be dimmed properly
+          if hl_group_info.hl_group_name == "LspInlayHint" then
+            local amt = 15
+            return r - amt, g - amt, b - amt
+          end
+
+          return r, g, b
+        end,
+      },
+    })
   end,
 }
