11eb6eb7a5e81186fe8cd6bf1caeb5be94c67029
Mika Vilpas <mika.vilpas@gmail.com>
2024-02-27 19:58:54 +0200

    refactor(nvim): typescript.lua configuration is a table of tables

--- .config/nvim/lua/plugins/typescript.lua
+++ .config/nvim/lua/plugins/typescript.lua
@@ -1,47 +1,49 @@
 ---@type LazySpec
 return {
-  "pmizio/typescript-tools.nvim",
-  ft = {
-    "typescript",
-    "javascript",
-    "typescriptreact",
-    "javascriptreact",
-  },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "neovim/nvim-lspconfig",
-    { "folke/neoconf.nvim", cmd = "Neoconf", dependencies = { "nvim-lspconfig" } },
+  {
+    "pmizio/typescript-tools.nvim",
+    ft = {
+      "typescript",
+      "javascript",
+      "typescriptreact",
+      "javascriptreact",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "neovim/nvim-lspconfig",
+      { "folke/neoconf.nvim", cmd = "Neoconf", dependencies = { "nvim-lspconfig" } },
 
-    {
-      "marilari88/twoslash-queries.nvim",
-      -- Usage
-      -- Write a '//    ^?' placing the sign '^' under the variable to inspected
-      opts = {
+      {
+        "marilari88/twoslash-queries.nvim",
+        -- Usage
+        -- Write a '//    ^?' placing the sign '^' under the variable to inspected
+        opts = {
 
-        -- https://github.com/marilari88/twoslash-queries.nvim?tab=readme-ov-file#config
-        multi_line = true, -- to print types in multi line mode
-        is_enabled = true, -- to keep disabled at startup and enable it on request with the TwoslashQueriesEnable
-        highlight = "@comment.note", -- to set up a highlight group for the virtual text
+          -- https://github.com/marilari88/twoslash-queries.nvim?tab=readme-ov-file#config
+          multi_line = true, -- to print types in multi line mode
+          is_enabled = true, -- to keep disabled at startup and enable it on request with the TwoslashQueriesEnable
+          highlight = "@comment.note", -- to set up a highlight group for the virtual text
+        },
       },
     },
-  },
 
-  opts = {
-    on_attach = function(client, bufnr)
-      require("twoslash-queries").attach(client, bufnr)
-    end,
+    opts = {
+      on_attach = function(client, bufnr)
+        require("twoslash-queries").attach(client, bufnr)
+      end,
 
-    settings = {
-      expose_as_code_action = "all",
+      settings = {
+        expose_as_code_action = "all",
 
-      tsserver_file_preferences = {
-        includeInlayParameterNameHints = "all",
-        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayVariableTypeHints = true,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayFunctionLikeReturnTypeHints = true,
-        includeInlayEnumMemberValueHints = true,
+        tsserver_file_preferences = {
+          includeInlayParameterNameHints = "all",
+          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        },
       },
     },
   },
