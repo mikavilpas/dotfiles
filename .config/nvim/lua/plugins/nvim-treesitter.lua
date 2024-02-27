c385bc5211185cc3571500b48e9f82e05202273c
Mika Vilpas <mika.vilpas@gmail.com>
2024-02-27 11:08:03 +0200

    fix(nvim): re-enable ii (inner indentation level) text objects

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -55,12 +55,6 @@ return {
               ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
               ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
 
-              ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
-              ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
-
-              ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
-              ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
-
               ["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
               ["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
 
