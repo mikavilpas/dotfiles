6f213f2f832bf15b571939917068e48d7f5855b7
Mika Vilpas <mika.vilpas@gmail.com>
2024-02-27 16:25:08 +0200

    feat(nvim): files are opened in the active nvim split when embedded
    This reverts commit 11c338c08e79015cd791e7c8557c39000477b68e.

--- .config/nvim/lua/plugins/terminal-integration.lua
+++ .config/nvim/lua/plugins/terminal-integration.lua
@@ -1,89 +1,92 @@
 ---@type LazySpec
 return {
-  -- Open files and command output from wezterm, kitty, and neovim terminals in
-  -- your current neovim instance
-  -- https://github.com/willothy/flatten.nvim
-  "willothy/flatten.nvim",
+  {
+    -- Open files and command output from wezterm, kitty, and neovim terminals in
+    -- your current neovim instance
+    -- https://github.com/willothy/flatten.nvim
+    "willothy/flatten.nvim",
 
-  -- Ensure that it runs first to minimize delay when opening file from terminal
-  lazy = false,
-  priority = 1001,
+    -- Ensure that it runs first to minimize delay when opening file from terminal
+    lazy = false,
+    priority = 1001,
 
-  -- disable for headless and embedded neovim
-  enabled = function()
-    for _, value in ipairs(vim.v.argv) do
-      if value == "--headless" or value == "--embed" then
-        return false
-      end
-    end
+    -- disable for headless and embedded neovim
+    enabled = function()
+      -- TODO disable this when debugging. Currently needs to be done manually
+      -- for _, value in ipairs(vim.v.argv) do
+      --   if value == "--headless" or value == "--embed" then
+      --     return false
+      --   end
+      -- end
 
-    return true
-  end,
+      return true
+    end,
 
-  opts = function()
-    ---@type Terminal?
-    local saved_terminal
+    opts = function()
+      ---@type Terminal?
+      local saved_terminal
 
-    return {
-      window = {
-        open = "alternate",
-      },
-      callbacks = {
-        should_block = function(argv)
-          -- -- Note that argv contains all the parts of the CLI command, including
-          -- -- Neovim's path, commands, options and files.
-          -- -- See: :help v:argv
-          --
-          -- -- In this case, we would block if we find the `-b` flag
-          -- -- This allows you to use `nvim -b file1` instead of
-          -- -- `nvim --cmd 'let g:flatten_wait=1' file1`
-          -- return vim.tbl_contains(argv, "-b")
-          --
-          -- -- Alternatively, we can block if we find the diff-mode option
-          -- -- return vim.tbl_contains(argv, "-d")
-        end,
-        pre_open = function()
-          -- local term = require("toggleterm.terminal")
-          -- local termid = term.get_focused_id()
-          -- saved_terminal = term.get(termid)
-        end,
-        post_open = function(bufnr, winnr, ft, is_blocking)
-          -- if is_blocking and saved_terminal then
-          --   -- Hide the terminal while it's blocking
-          --   saved_terminal:close()
-          -- else
-          --   -- If it's a normal file, just switch to its window
-          --   vim.api.nvim_set_current_win(winnr)
-          --
-          --   -- If we're in a different wezterm pane/tab, switch to the current one
-          --   -- Requires willothy/wezterm.nvim
-          --   require("wezterm").switch_pane.id(tonumber(os.getenv("WEZTERM_PANE")))
-          -- end
-          --
-          -- -- If the file is a git commit, create one-shot autocmd to delete its buffer on write
-          -- -- If you just want the toggleable terminal integration, ignore this bit
-          -- if ft == "gitcommit" or ft == "gitrebase" then
-          --   vim.api.nvim_create_autocmd("BufWritePost", {
-          --     buffer = bufnr,
-          --     once = true,
-          --     callback = vim.schedule_wrap(function()
-          --       vim.api.nvim_buf_delete(bufnr, {})
-          --     end),
-          --   })
-          -- end
-        end,
-        block_end = function()
-          -- After blocking ends (for a git commit, etc), reopen the terminal
-          vim.schedule(function()
-            if saved_terminal then
-              saved_terminal:open()
-              saved_terminal = nil
-            end
-          end)
-        end,
-      },
-    }
-  end,
+      return {
+        window = {
+          open = "alternate",
+        },
+        callbacks = {
+          should_block = function(argv)
+            -- -- Note that argv contains all the parts of the CLI command, including
+            -- -- Neovim's path, commands, options and files.
+            -- -- See: :help v:argv
+            --
+            -- -- In this case, we would block if we find the `-b` flag
+            -- -- This allows you to use `nvim -b file1` instead of
+            -- -- `nvim --cmd 'let g:flatten_wait=1' file1`
+            -- return vim.tbl_contains(argv, "-b")
+            --
+            -- -- Alternatively, we can block if we find the diff-mode option
+            -- -- return vim.tbl_contains(argv, "-d")
+          end,
+          pre_open = function()
+            -- local term = require("toggleterm.terminal")
+            -- local termid = term.get_focused_id()
+            -- saved_terminal = term.get(termid)
+          end,
+          post_open = function(bufnr, winnr, ft, is_blocking)
+            -- if is_blocking and saved_terminal then
+            --   -- Hide the terminal while it's blocking
+            --   saved_terminal:close()
+            -- else
+            --   -- If it's a normal file, just switch to its window
+            --   vim.api.nvim_set_current_win(winnr)
+            --
+            --   -- If we're in a different wezterm pane/tab, switch to the current one
+            --   -- Requires willothy/wezterm.nvim
+            --   require("wezterm").switch_pane.id(tonumber(os.getenv("WEZTERM_PANE")))
+            -- end
+            --
+            -- -- If the file is a git commit, create one-shot autocmd to delete its buffer on write
+            -- -- If you just want the toggleable terminal integration, ignore this bit
+            -- if ft == "gitcommit" or ft == "gitrebase" then
+            --   vim.api.nvim_create_autocmd("BufWritePost", {
+            --     buffer = bufnr,
+            --     once = true,
+            --     callback = vim.schedule_wrap(function()
+            --       vim.api.nvim_buf_delete(bufnr, {})
+            --     end),
+            --   })
+            -- end
+          end,
+          block_end = function()
+            -- After blocking ends (for a git commit, etc), reopen the terminal
+            vim.schedule(function()
+              if saved_terminal then
+                saved_terminal:open()
+                saved_terminal = nil
+              end
+            end)
+          end,
+        },
+      }
+    end,
+  },
 
   {
     "akinsho/toggleterm.nvim",
