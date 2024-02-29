f9a5aa4a75c59f27b0281f418104355a0de33684
Mika Vilpas <mika.vilpas@gmail.com>
2024-02-29 21:02:50 +0200

    feat(nvim): enter in normal mode toggles alt formatting for current ts node

--- .config/nvim/lua/plugins/ts-node-action.lua
+++ .config/nvim/lua/plugins/ts-node-action.lua
@@ -1,9 +1,26 @@
 ---@type LazySpec
 return {
-  "ckolkey/ts-node-action",
-  dependencies = { "nvim-treesitter" },
-  opts = {},
-  config = function()
-    require("ts-node-action").setup()
-  end,
+  {
+    "ckolkey/ts-node-action",
+    dependencies = { "nvim-treesitter" },
+    opts = {},
+  },
+  {
+    -- https://github.com/Wansmer/treesj
+    -- Neovim plugin for splitting/joining blocks of code
+
+    "Wansmer/treesj",
+    keys = {
+      {
+        "<enter>",
+        function()
+          require("treesj").toggle()
+        end,
+      },
+    },
+    opts = {
+      use_default_keymaps = false,
+    },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+  },
 }
