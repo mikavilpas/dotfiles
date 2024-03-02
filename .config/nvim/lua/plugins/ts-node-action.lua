fe4683ac2fc3502f371a9c0864b7ae5f8cda1cb6
Mika Vilpas <mika.vilpas@gmail.com>
2024-03-02 15:36:29 +0200

    feat(nvim): expand and collapse an html tag
    feat(nvim): "expand tag" formats result
    
    WIP add name
    
    WIP can collapse an empty tag

--- .config/nvim/lua/plugins/ts-node-action.lua
+++ .config/nvim/lua/plugins/ts-node-action.lua
@@ -6,7 +6,71 @@ return {
     "ckolkey/ts-node-action",
     event = "BufRead",
     dependencies = { "nvim-treesitter" },
-    opts = {},
+    config = function()
+      local helpers = require("ts-node-action.helpers")
+
+      local opts = {
+        html = {
+          self_closing_tag = {
+            {
+              --- @param node TSNode
+              function(node)
+                local content_nodes = helpers.destructure_node(node)
+                local tag_name = content_nodes["tag_name"]
+
+                local node_text = helpers.node_text(node)
+
+                if type(node_text) == "table" then
+                  local last_line = node_text[#node_text]
+                  last_line = last_line:gsub("%s*/>$", "></" .. tag_name .. ">")
+                  node_text[#node_text] = last_line
+
+                  return node_text, { format = true }
+                end
+
+                -- single line
+                ---@diagnostic disable-next-line: need-check-nil
+                local text = node_text:gsub("%s*/>$", "></" .. tag_name .. ">")
+                return text, { format = true }
+              end,
+              name = "Expand tag",
+            },
+          },
+
+          start_tag = {
+            {
+              ---@param node TSNode
+              function(node)
+                -- if the tag has contents, we can't collapse it
+                local parent_tag = node:parent()
+
+                if not parent_tag or parent_tag:child_count() > 2 then
+                  return
+                end
+
+                local text = helpers.node_text(node)
+
+                if type(text) == "table" then
+                  local last_line = text[#text]
+                  last_line = last_line:gsub(">$", " />")
+                  text[#text] = last_line
+
+                  return text, { format = true, target = parent_tag }
+                end
+
+                -- single line
+                ---@diagnostic disable-next-line: need-check-nil
+                text = text:gsub(">$", " />")
+                return text, { format = true, target = parent_tag }
+              end,
+              name = "Collapse empty tag",
+            },
+          },
+        },
+      }
+
+      require("ts-node-action").setup(opts)
+    end,
     keys = {
       {
         "<leader><enter>",
