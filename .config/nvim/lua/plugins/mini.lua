0e7b9dd6db9eeb5709264440c5e158a7e5733fc6
Mika Vilpas <mika.vilpas@gmail.com>
2024-03-04 21:44:27 +0200

    fix(nvim): inconsistent behavior for indent text objects

---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,30 @@
+---@type LazySpec
+return {
+  {
+    "echasnovski/mini.indentscope",
+    -- https://github.com/echasnovski/mini.indentscope
+    opts = {
+      -- Module mappings. Use `''` (empty string) to disable one.
+      mappings = {
+        -- Textobjects
+        object_scope = "",
+        object_scope_with_border = "",
+
+        -- Motions (jump to respective border line; if not present - body line)
+        goto_top = "",
+        goto_bottom = "",
+      },
+    },
+  },
+  {
+    -- Usage
+    -- This plugin defines two new text objects. These are very similar - they differ only in whether they include the line below the block or not.
+    -- Key bindings	Description
+    -- <count>ai An Indentation level and line above.
+    -- <count>ii Inner Indentation level (no line above).
+    -- <count>aI An Indentation level and lines above/below.
+    -- <count>iI Inner Indentation level (no lines above/below).
+    "michaeljsmith/vim-indent-object",
+    event = "BufRead",
+  },
+}
