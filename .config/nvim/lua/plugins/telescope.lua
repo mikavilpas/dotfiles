0f16d7dd8f7cab5711802a6aceee77e8008a7774
Mika Vilpas <mika.vilpas@gmail.com>
2024-03-06 14:36:29 +0200

    refactor(nvim): telescope is a table of tables

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,131 +1,133 @@
 ---@type LazySpec
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    "Marskey/telescope-sg",
-    "nvim-lua/plenary.nvim",
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "Marskey/telescope-sg",
+      "nvim-lua/plenary.nvim",
 
-    -- https://github.com/debugloop/telescope-undo.nvim
-    --
-    -- Usage:
-    -- <leader>sc (search commands), then Telescope undo
-    "debugloop/telescope-undo.nvim",
-    "smartpde/telescope-recent-files",
+      -- https://github.com/debugloop/telescope-undo.nvim
+      --
+      -- Usage:
+      -- <leader>sc (search commands), then Telescope undo
+      "debugloop/telescope-undo.nvim",
+      "smartpde/telescope-recent-files",
 
-    -- native telescope bindings to zf for sorting results.
-    -- In short, zf is a filepath fuzzy finder. It is designed for better
-    -- matching on filepaths than fzf or fzy. Matches on filenames are
-    -- prioritized, and the strict path matching feature helps narrow down
-    -- directory trees with precision. See the zf repo for full details.
-    -- https://github.com/natecraddock/telescope-zf-native.nvim
-    "natecraddock/telescope-zf-native.nvim",
+      -- native telescope bindings to zf for sorting results.
+      -- In short, zf is a filepath fuzzy finder. It is designed for better
+      -- matching on filepaths than fzf or fzy. Matches on filenames are
+      -- prioritized, and the strict path matching feature helps narrow down
+      -- directory trees with precision. See the zf repo for full details.
+      -- https://github.com/natecraddock/telescope-zf-native.nvim
+      "natecraddock/telescope-zf-native.nvim",
 
-    -- https://github.com/AckslD/nvim-neoclip.lua
-    {
-      "AckslD/nvim-neoclip.lua",
-      dependencies = { "kkharji/sqlite.lua" },
-      opts = {
-        default_register = { "+" },
-        enable_persistent_history = true,
+      -- https://github.com/AckslD/nvim-neoclip.lua
+      {
+        "AckslD/nvim-neoclip.lua",
+        dependencies = { "kkharji/sqlite.lua" },
+        opts = {
+          default_register = { "+" },
+          enable_persistent_history = true,
+        },
+        event = "VeryLazy",
       },
-      event = "VeryLazy",
     },
-  },
-  keys = {
-    { "<leader><leader>", false },
-    {
-      "<leader>ff",
-      require("my-telescope-searches").my_find_file_in_project,
-      { desc = "Find files (including in git submodules)" },
-    },
-    {
-      -- mnemonic: "paste"
-      "<leader>p",
-      function()
-        --
-        require("telescope").extensions.neoclip.default({})
-      end,
-      { desc = "Paste with telescope (neoclip)" },
-    },
-    {
-      "<leader>/",
-      mode = { "n", "v" },
-      function()
-        require("my-telescope-searches").my_live_grep()
-      end,
-      desc = "search project 🤞🏻",
-    },
-    {
-      "<leader>fr",
-      mode = { "n", "v" },
-      function()
-        require("telescope").extensions.recent_files.pick({
-          only_cwd = true,
-        })
-      end,
-      desc = "search recent_files (cwd)",
-    },
-    {
-      "<leader>fR",
-      mode = { "n", "v" },
-      function()
-        require("telescope").extensions.recent_files.pick({
-          only_cwd = false,
-        })
-      end,
-      desc = "search recent_files (global)",
-    },
-  },
-
-  opts = {
-    defaults = {
-      layout_strategy = "horizontal",
-      layout_config = { prompt_position = "top" },
-      sorting_strategy = "ascending",
-      winblend = 0,
-
-      mappings = {
-        n = {
-          ["<C-y>"] = require("my-telescope-searches").my_copy_relative_path,
-        },
-        i = {
-          ["<C-y>"] = require("my-telescope-searches").my_copy_relative_path,
-        },
+    keys = {
+      { "<leader><leader>", false },
+      {
+        "<leader>ff",
+        require("my-telescope-searches").my_find_file_in_project,
+        { desc = "Find files (including in git submodules)" },
+      },
+      {
+        -- mnemonic: "paste"
+        "<leader>p",
+        function()
+          --
+          require("telescope").extensions.neoclip.default({})
+        end,
+        { desc = "Paste with telescope (neoclip)" },
+      },
+      {
+        "<leader>/",
+        mode = { "n", "v" },
+        function()
+          require("my-telescope-searches").my_live_grep()
+        end,
+        desc = "search project 🤞🏻",
+      },
+      {
+        "<leader>fr",
+        mode = { "n", "v" },
+        function()
+          require("telescope").extensions.recent_files.pick({
+            only_cwd = true,
+          })
+        end,
+        desc = "search recent_files (cwd)",
+      },
+      {
+        "<leader>fR",
+        mode = { "n", "v" },
+        function()
+          require("telescope").extensions.recent_files.pick({
+            only_cwd = false,
+          })
+        end,
+        desc = "search recent_files (global)",
       },
     },
 
-    extensions = {
-      undo = {
-        side_by_side = true,
-        layout_strategy = "vertical",
-        layout_config = {
-          preview_height = 0.8,
-        },
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
 
         mappings = {
           n = {
-            ["<cr>"] = function(bufnr)
-              return require("telescope-undo.actions").restore(bufnr)
-            end,
+            ["<C-y>"] = require("my-telescope-searches").my_copy_relative_path,
           },
           i = {
-            ["<cr>"] = function(bufnr)
-              return require("telescope-undo.actions").restore(bufnr)
-            end,
+            ["<C-y>"] = require("my-telescope-searches").my_copy_relative_path,
+          },
+        },
+      },
+
+      extensions = {
+        undo = {
+          side_by_side = true,
+          layout_strategy = "vertical",
+          layout_config = {
+            preview_height = 0.8,
+          },
+
+          mappings = {
+            n = {
+              ["<cr>"] = function(bufnr)
+                return require("telescope-undo.actions").restore(bufnr)
+              end,
+            },
+            i = {
+              ["<cr>"] = function(bufnr)
+                return require("telescope-undo.actions").restore(bufnr)
+              end,
+            },
           },
         },
       },
     },
-  },
 
-  config = function(_, opts)
-    -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
-    -- configs for us. We won't use data, as everything is in it's own namespace (telescope
-    -- defaults, as well as each extension).
-    local telescope = require("telescope")
-    telescope.setup(opts)
-    telescope.load_extension("undo")
-    telescope.load_extension("recent_files")
-    telescope.load_extension("neoclip")
-  end,
+    config = function(_, opts)
+      -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
+      -- configs for us. We won't use data, as everything is in it's own namespace (telescope
+      -- defaults, as well as each extension).
+      local telescope = require("telescope")
+      telescope.setup(opts)
+      telescope.load_extension("undo")
+      telescope.load_extension("recent_files")
+      telescope.load_extension("neoclip")
+    end,
+  },
 }
