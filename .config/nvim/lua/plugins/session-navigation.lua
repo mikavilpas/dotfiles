ece897794e1b92a117395530a7753d592878e226
Mika Vilpas <mika.vilpas@gmail.com>
2024-03-10 20:57:29 +0200

    feat(nvim): switch back to harpoon2

--- .config/nvim/lua/plugins/session-navigation.lua
+++ .config/nvim/lua/plugins/session-navigation.lua
@@ -1,10 +1,47 @@
 ---@type LazySpec
 return {
   {
-    "otavioschwanck/arrow.nvim",
-    opts = {
-      show_icons = true,
-      leader_key = ";", -- Recommended to be a single key
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
     },
+
+    keys = {
+      {
+        "<leader>he",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Open harpoon window",
+      },
+      {
+        "<leader>ha",
+        function()
+          require("harpoon"):list():append()
+        end,
+        desc = "Harpoon add file",
+      },
+      {
+        "ö",
+        function()
+          require("harpoon"):list():prev()
+        end,
+        desc = "Harpoon previous file",
+      },
+      {
+        "ä",
+        function()
+          require("harpoon"):list():next()
+        end,
+        desc = "Harpoon next file",
+      },
+    },
+
+    config = function()
+      require("harpoon"):setup({
+      })
+    end,
   },
 }
