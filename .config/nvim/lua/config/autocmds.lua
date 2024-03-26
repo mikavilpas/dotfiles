15fd8141cc56038dc68617132dc9a353475ba87f
Mika Vilpas <mika.vilpas@gmail.com>
2024-03-26 19:20:45 +0200

    feat(nvim): when COMMIT_EDITMSG file is closed, display lazygit

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- if the git COMMIT_EDITMSG file is closed, automatically display lazygit
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    local file = vim.fn.expand("<afile>")
+    if file:match("COMMIT_EDITMSG") then
+      _G.openLazyGit()
+    end
+  end,
+})
