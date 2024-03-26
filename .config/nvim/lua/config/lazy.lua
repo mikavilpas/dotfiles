95aaff8f3adc2ba3e32658f400397c480b3f9ef2
Mika Vilpas <mika.vilpas@gmail.com>
2024-03-26 18:17:22 +0200

    perf(nvim): disable more plugins, not sure if this has any effect

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -47,9 +47,9 @@ require("lazy").setup({
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "netrwPlugin",
-        -- "matchparen",
+        "matchit",
+        "netrwPlugin",
+        "matchparen",
         "tarPlugin",
         "tohtml",
         "tutor",
