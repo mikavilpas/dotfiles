5774828e711ff262751167f6123a45898e460259
Mika Vilpas <mika.vilpas@gmail.com>
2024-04-13 20:38:58 +0300

    feat(nvim): nicer colors and doc size for lsp_signature

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -79,6 +79,9 @@ return {
       opts = {
         -- https://github.com/ray-x/lsp_signature.nvim?tab=readme-ov-file#full-configuration-with-default-values
         bind = true, -- This is mandatory, otherwise border config won't get registered.
+        max_height = 999,
+        doc_lines = 999,
+        hi_parameter = "@text.note",
         handler_opts = { border = "rounded" },
       },
       config = function(_, opts)
