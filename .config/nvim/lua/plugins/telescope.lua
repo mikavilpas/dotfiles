a40bfdfed73888b177f041e638b2d688ffd2ddd8
Mika Vilpas <mika.vilpas@gmail.com>
2024-04-21 16:15:21 +0300

    feat(nvim): telescope shows files first, directories second
    https://github.com/nvim-telescope/telescope.nvim/pull/3010

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -73,6 +73,11 @@ return {
         layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending",
         winblend = 0,
+        path_display = {
+          filename_first = {
+            reverse_directories = false,
+          },
+        },
 
         mappings = {
           n = {
