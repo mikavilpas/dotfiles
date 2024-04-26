3b5bc69917ee7e808eef0fc9734693d0141ea474
Mika Vilpas <mika.vilpas@gmail.com>
2024-04-26 20:25:50 +0300

    feat(nvim): allow yazi.nvim to open directories with "nvim ."

--- .config/nvim/lua/plugins/my-file-manager.lua
+++ .config/nvim/lua/plugins/my-file-manager.lua
@@ -1,30 +1,40 @@
 ---@type LazySpec
 return {
-  "mikavilpas/yazi.nvim",
-  -- dir = "~/git/yazi.nvim/",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  event = "VeryLazy",
-  keys = {
-    {
-      "<up>",
-      function()
-        require("yazi").yazi()
-      end,
-      desc = "Open the file manager",
+  {
+    "mikavilpas/yazi.nvim",
+    -- dir = "~/git/yazi.nvim/",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
     },
-    {
-      "<s-up>",
-      function()
-        require("yazi").yazi(nil, vim.fn.getcwd())
-      end,
-      desc = "Open the file manager in the cwd",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<up>",
+        function()
+          require("yazi").yazi()
+        end,
+        desc = "Open the file manager",
+      },
+      {
+        "<s-up>",
+        function()
+          require("yazi").yazi(nil, vim.fn.getcwd())
+        end,
+        desc = "Open the file manager in the cwd",
+      },
+    },
+    ---@type YaziConfig
+    opts = {
+      open_for_directories = true,
+      enable_mouse_support = true,
     },
   },
-  ---@type YaziConfig
-  opts = {
-    open_for_directories = true,
-    enable_mouse_support = true,
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        hijack_netrw_behavior = "disabled",
+      },
+    },
   },
 }
