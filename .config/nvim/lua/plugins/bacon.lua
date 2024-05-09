b2587d0c258ff4c0a822d165d51c5f3a0a06ca2f
Mika Vilpas <mika.vilpas@gmail.com>
2024-05-09 19:37:57 +0300

    feat(bacon): share errors to neovim

---
+++ .config/nvim/lua/plugins/bacon.lua
@@ -0,0 +1,45 @@
+---@type LazySpec
+return {
+  -- "Canop/nvim-bacon",
+  dir = "~/git/nvim-bacon/",
+  -- cmd = { "BaconLoad", "BaconShow", "BaconList", "BaconPrevious", "BaconNext" },
+  opts = {
+    quickfix = {
+      enabled = true, -- true to populate the quickfix list with bacon errors and warnings
+      event_trigger = true, -- triggers the QuickFixCmdPost event after populating the quickfix list
+    },
+  },
+  keys = {
+    {
+      "<leader>!",
+      function()
+        local bacon = require("bacon")
+        bacon.bacon_load()
+        bacon.bacon_next()
+        vim.cmd("copen")
+      end,
+    },
+  },
+  -- ft = { "rs" }, -- rust
+  -- config = function()
+  --   require("bacon").setup({
+  --     quickfix = {
+  --       enabled = true,
+  --       event_trigger = true,
+  --     },
+  --   })
+  -- end,
+  -- keys = {
+  --   {
+  --     "<space>.",
+  --     cmd = function()
+  --       vim.cmd("BaconLoad")
+  --       vim.cmd("write")
+  --       vim.cmd("BaconNext")
+  --     end,
+  --   },
+  --   {
+  --     "<space>t",
+  --     cmd = "BaconList",
+  --   },
+}
