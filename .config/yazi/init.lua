2d9f055503f2612b5d7f8e1de6012397293bf5b4
Mika Vilpas <mika.vilpas@gmail.com>
2024-05-27 16:46:31 +0300

    feat(yazi): enable cross instance yank-paste

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,3 +1,9 @@
 -- Starship prompt plugin for yazi
 -- https://github.com/Rolv-Apneseth/starship.yazi
 require("starship"):setup()
+
+-- This plugin provides cross-instance yank ability, which means you can yank
+-- files in one instance, and then paste them in another instance.
+require("session"):setup({
+	sync_yanked = true,
+})
