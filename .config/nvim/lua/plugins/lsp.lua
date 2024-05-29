6a89828b4b8e4317a91ae8739ef699c94bf619a7
Mika Vilpas <mika.vilpas@gmail.com>
2024-05-29 11:06:22 +0300

    feat(nvim): don't suppress that many noice messages

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -45,34 +45,11 @@ return {
         routes = {
           -- disable some annoying messages popping up. I hate them!
           -- stylua: ignore start
-          { view = "split", filter = { event = "msg_show", min_height = 3 } },
-          { filter = { find = "No information available" }, opts = { stop = true } },
-          { filter = { find = "fewer lines;" }, opts = { skip = true } },
-          { filter = { find = "more line;" }, opts = { skip = true } },
-          { filter = { find = "more lines;" }, opts = { skip = true } },
-          { filter = { find = "less;" }, opts = { skip = true } },
-          { filter = { find = "change;" }, opts = { skip = true } },
-          { filter = { find = "changes;" }, opts = { skip = true } },
-          { filter = { find = "indent" }, opts = { skip = true } },
-          { filter = { find = "move" }, opts = { skip = true } },
 
           -- Disable "file_path" AMOUNT_OF_LINESL, AMOUNT_OF_BYTESB message
           -- https://github.com/KoalaVim/KoalaVim/blob/e4649e74a1838e6a4a1a55a72e58280064bb909c/lua/KoalaVim/misc/noice_routes.lua#L6
           { filter = { event = "msg_show", kind = "", find = '"[%w%p]+" %d+L, %d+B' }, opts = { skip = true } },
 
-          -- Disable "search messages"
-          { filter = { event = "msg_show", kind = "wmsg", find = "search hit BOTTOM, continuing at TOP", }, opts = { skip = true }, },
-          { filter = { event = "msg_show", kind = "wmsg", find = "search hit TOP, continuing at BOTTOM" }, opts = { skip = true }, },
-
-          -- Disable "redo/undo" messages
-          { filter = { event = "msg_show", kind = "lua_error", find = "more line" }, opts = { skip = true } },
-          { filter = { event = "msg_show", kind = "lua_error", find = "fewer line" }, opts = { skip = true } },
-          { filter = { event = "msg_show", kind = "lua_error", find = "line less" }, opts = { skip = true } },
-          { filter = { event = "msg_show", kind = "lua_error", find = "change;" }, opts = { skip = true } },
-          { filter = { event = "msg_show", kind = "", find = "more line" }, opts = { skip = true } },
-          { filter = { event = "msg_show", kind = "", find = "fewer line" }, opts = { skip = true } },
-          { filter = { event = "msg_show", kind = "", find = "line less" }, opts = { skip = true } },
-          { filter = { event = "msg_show", kind = "", find = "change;" }, opts = { skip = true } },
           -- stylua: ignore end
         },
       },
