91c26e3b3b1d2d9b3eab7ea872484625fcc315bb
Mika Vilpas <mika.vilpas@gmail.com>
2024-05-30 18:43:50 +0300

    feat(nvim): noice silences more annoying messages again

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -24,6 +24,16 @@ return {
           -- disable some annoying messages popping up. I hate them!
           -- stylua: ignore start
 
+          { filter = { find = "No information available" }, opts = { stop = true } },
+          { filter = { find = "fewer lines;" }, opts = { skip = true } },
+          { filter = { find = "more line;" }, opts = { skip = true } },
+          { filter = { find = "more lines;" }, opts = { skip = true } },
+          { filter = { find = "less;" }, opts = { skip = true } },
+          { filter = { find = "change;" }, opts = { skip = true } },
+          { filter = { find = "changes;" }, opts = { skip = true } },
+          { filter = { find = "indent" }, opts = { skip = true } },
+          { filter = { find = "move" }, opts = { skip = true } },
+
           -- Disable "file_path" AMOUNT_OF_LINESL, AMOUNT_OF_BYTESB message
           -- https://github.com/KoalaVim/KoalaVim/blob/e4649e74a1838e6a4a1a55a72e58280064bb909c/lua/KoalaVim/misc/noice_routes.lua#L6
           { filter = { event = "msg_show", kind = "", find = '"[%w%p]+" %d+L, %d+B' }, opts = { skip = true } },
