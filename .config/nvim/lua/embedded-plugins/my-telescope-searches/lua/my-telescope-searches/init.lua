3cabbb0fec415f1227a37f3bf5b2c751ac44ceaf
Mika Vilpas <mika.vilpas@gmail.com>
2024-05-31 22:11:00 +0300

    feat(neovim): my-telescope-searches show the dir in title
    They used to vim.notify but that is not necessary

--- .config/nvim/lua/embedded-plugins/my-telescope-searches/lua/my-telescope-searches/init.lua
+++ .config/nvim/lua/embedded-plugins/my-telescope-searches/lua/my-telescope-searches/init.lua
@@ -72,9 +72,9 @@ function M.my_find_file_in_project()
   local cwd = M.find_project_root()
   local selection = M.get_visual()
 
-  vim.notify("searching in " .. cwd)
   require("telescope.builtin").find_files({
     find_command = { "fd", "--hidden" },
+    prompt_title = "Find files in " .. cwd,
     cwd = cwd,
     search_file = selection,
   })
@@ -88,12 +88,11 @@ function M.my_live_grep(options)
   local cwd = (options or {}).cwd or M.find_project_root()
   local selection = M.get_visual()
 
-  vim.notify("searching in " .. cwd)
-
   -- pro tip: search for an initial, wide result with this, and then hit
   -- c-spc to use fuzzy matching to narrow it down
   require("telescope.builtin").live_grep({
     cwd = cwd,
+    prompt_title = "Live grep in " .. cwd,
     default_text = selection,
     only_sort_text = true,
     additional_args = function()
