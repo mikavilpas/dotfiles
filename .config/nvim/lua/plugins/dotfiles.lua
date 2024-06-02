176fcd890f23049e7af17a17aabbc27a4f642862
Mika Vilpas <mika.vilpas@gmail.com>
2024-06-02 22:31:12 +0300

    refactor(dotfiles): remove submodules and manage them with lazy.nvim
    This is the same approach as in
    https://github.com/mikavilpas/yazi.nvim/blob/master/documentation/plugin-manager.md

---
+++ .config/nvim/lua/plugins/dotfiles.lua
@@ -0,0 +1,18 @@
+---@type LazySpec
+return {
+  {
+    "ohmyzsh/ohmyzsh",
+    lazy = true,
+    build = function(self)
+      require("yazi.plugin").symlink(self, vim.fn.expand("~/dotfiles/.oh-my-zsh"))
+    end,
+  },
+
+  {
+    "catppuccin/bat",
+    lazy = true,
+    build = function(self)
+      require("yazi.plugin").symlink(self, vim.fn.expand("~/dotfiles/.config/bat/themes/catppuccin-bat"))
+    end,
+  },
+}
