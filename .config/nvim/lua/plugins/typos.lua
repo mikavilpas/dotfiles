090f0236a50b4509ac9de5e98a529bfe8aa62a3b
Mika Vilpas <mika.vilpas@gmail.com>
2024-06-06 17:12:53 +0300

    feat(nvim): add dev setup for typos-lsp development

--- .config/nvim/lua/plugins/typos.lua
+++ .config/nvim/lua/plugins/typos.lua
@@ -33,7 +33,22 @@ return {
             "rust",
             "markdown",
             "toml",
+            "text", -- txt
           },
+
+          init_options = {
+            -- Custom config. Used together with any workspace config files, taking precedence for
+            -- settings declared in both. Equivalent to the typos `--config` cli argument.
+            -- config = '~/code/typos-lsp/crates/typos-lsp/tests/typos.toml',
+            -- How typos are rendered in the editor, eg: as errors, warnings, information, or hints.
+            -- Defaults to error.
+            diagnosticSeverity = "HINT",
+          },
+
+          -- These settings are useful when I develop this LSP
+          -- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
+          cmd_env = { RUST_LOG = "debug" },
+          cmd = { "/Users/mikavilpas/git/typos-lsp/target/debug/typos-lsp" },
         },
       },
     },
