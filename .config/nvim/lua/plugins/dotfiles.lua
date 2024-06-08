6b5210547d76c636c66e76f9a28b4f0e60dc5f70
Mika Vilpas <mika.vilpas@gmail.com>
2024-06-08 09:20:22 +0300

    fix(nvim): make sure catppuccin/bat is added to cache

--- .config/nvim/lua/plugins/dotfiles.lua
+++ .config/nvim/lua/plugins/dotfiles.lua
@@ -10,9 +10,11 @@ return {
 
   {
     "catppuccin/bat",
+    name = "catppuccin-bat",
     lazy = true,
     build = function(self)
       require("yazi.plugin").symlink(self, vim.fn.expand("~/dotfiles/.config/bat/themes/catppuccin-bat"))
+      vim.system({ "bat cache --build" }, { text = true })
     end,
   },
 }
