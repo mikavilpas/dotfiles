544e90168e375ac0fd55ccd2507861e25372e145
Mika Vilpas <mika.vilpas@gmail.com>
2024-07-13 19:45:34 +0300

    fix(nvim): annoying lazyvim keymap prevents using esc in terminal mode

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,8 @@
 -- Add any additional keymaps here
 --
 
+vim.keymap.set({ "t" }, "<esc><esc>", "<Nop>")
+
 vim.api.nvim_create_autocmd("BufEnter", {
   callback = function()
     vim.opt.formatoptions:remove({ "c", "r", "o" })
