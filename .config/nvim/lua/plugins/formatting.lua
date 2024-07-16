b9328a301adec69b0e1ded158473897643de3c06
Mika Vilpas <mika.vilpas@gmail.com>
2024-07-16 07:44:06 +0300

    feat(nvim): add undojoin to conform opts

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,3 +1,5 @@
+---@module "conform"
+
 -- This is this file before it was reverted to using prettier instead of prettierd
 -- https://github.com/LazyVim/LazyVim/blob/91126b9896bebcea9a21bce43be4e613e7607164/lua/lazyvim/plugins/extras/formatting/prettier.lua
 -- https://github.com/LazyVim/LazyVim/commit/57b504b9e8ae95c294c17e97e7f017f6f802ebbc?diff=split&w=0
@@ -21,7 +23,9 @@ return {
   {
     "stevearc/conform.nvim",
     optional = true,
+    ---@type conform.FormatOpts
     opts = {
+      undojoin = true,
       formatters_by_ft = {
         ["javascript"] = { { "prettierd" } },
         ["javascriptreact"] = { { "prettierd" } },
