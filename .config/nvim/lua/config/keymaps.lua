aee0d0c9b40d582ff2d7488d6122650697f0b726
Mika Vilpas <mika.vilpas@gmail.com>
2024-07-26 08:14:15 +0300

    feat(nvim): use `s` to trigger `nvim-surround` in visual mode

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,6 +6,8 @@
 vim.keymap.set({ "t" }, "<esc><esc>", "<Nop>")
 vim.keymap.set({ "n" }, "<leader>w", "<Nop>")
 
+vim.keymap.set({ "v" }, "s", "<Plug>(nvim-surround-visual)")
+
 vim.api.nvim_create_autocmd("BufEnter", {
   callback = function()
     vim.opt.formatoptions:remove({ "c", "r", "o" })
