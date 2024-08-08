819f6fd97ba34a1f57134d3dab5209da76d308ca
Mika Vilpas <mika.vilpas@gmail.com>
2024-08-08 10:10:22 +0300

    fix(nvim): hide annoying eslint error messages

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -21,24 +21,28 @@ return {
     },
 
     routes = {
-          -- disable some annoying messages popping up. I hate them!
-          -- stylua: ignore start
+      -- disable some annoying messages popping up. I hate them!
 
-          { filter = { find = "No information available" }, opts = { stop = true } },
-          { filter = { find = "fewer lines;" }, opts = { skip = true } },
-          { filter = { find = "more line;" }, opts = { skip = true } },
-          { filter = { find = "more lines;" }, opts = { skip = true } },
-          { filter = { find = "less;" }, opts = { skip = true } },
-          { filter = { find = "change;" }, opts = { skip = true } },
-          { filter = { find = "changes;" }, opts = { skip = true } },
-          { filter = { find = "indent" }, opts = { skip = true } },
-          { filter = { find = "move" }, opts = { skip = true } },
+      { filter = { find = "No information available" }, opts = { stop = true } },
+      { filter = { find = "fewer lines;" }, opts = { skip = true } },
+      { filter = { find = "more line;" }, opts = { skip = true } },
+      { filter = { find = "more lines;" }, opts = { skip = true } },
+      { filter = { find = "less;" }, opts = { skip = true } },
+      { filter = { find = "change;" }, opts = { skip = true } },
+      { filter = { find = "changes;" }, opts = { skip = true } },
+      { filter = { find = "indent" }, opts = { skip = true } },
+      { filter = { find = "move" }, opts = { skip = true } },
+      { filter = { find = "move" }, opts = { skip = true } },
+      {
+        filter = {
+          find = "eslint: -32603: Request textDocument/diagnostic failed with message: Could not find config file.",
+        },
+        opts = { skip = true },
+      },
 
-          -- Disable "file_path" AMOUNT_OF_LINESL, AMOUNT_OF_BYTESB message
-          -- https://github.com/KoalaVim/KoalaVim/blob/e4649e74a1838e6a4a1a55a72e58280064bb909c/lua/KoalaVim/misc/noice_routes.lua#L6
-          { filter = { event = "msg_show", kind = "", find = '"[%w%p]+" %d+L, %d+B' }, opts = { skip = true } },
-
-      -- stylua: ignore end
+      -- Disable "file_path" AMOUNT_OF_LINESL, AMOUNT_OF_BYTESB message
+      -- https://github.com/KoalaVim/KoalaVim/blob/e4649e74a1838e6a4a1a55a72e58280064bb909c/lua/KoalaVim/misc/noice_routes.lua#L6
+      { filter = { event = "msg_show", kind = "", find = '"[%w%p]+" %d+L, %d+B' }, opts = { skip = true } },
     },
   },
 }
