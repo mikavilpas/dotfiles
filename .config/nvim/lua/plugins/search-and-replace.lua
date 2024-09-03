3812bc4e0cb9524c0b01f558b853e64c70210682
Mika Vilpas <mika.vilpas@gmail.com>
2024-09-03 21:02:08 +0300

    feat(nvim): bring nvim-rip-substitute back

---
+++ .config/nvim/lua/plugins/search-and-replace.lua
@@ -0,0 +1,22 @@
+---@module "lazy"
+---@module "rip-substitute"
+
+---@type LazySpec
+return {
+  "chrisgrieser/nvim-rip-substitute",
+  cmd = "RipSubstitute",
+  ---@type ripSubstituteConfig
+  opts = {
+    prefill = { startInReplaceLineIfPrefill = true },
+  },
+  keys = {
+    {
+      "<leader>r",
+      function()
+        require("rip-substitute").sub()
+      end,
+      mode = { "n", "x" },
+      desc = " rip substitute",
+    },
+  },
+}
