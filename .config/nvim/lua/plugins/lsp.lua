bf5554998b797a5a7511c7d98d4e06fbd1b5589a
Mika Vilpas <mika.vilpas@gmail.com>
2024-09-09 20:58:19 +0300

    feat(nvim): add garbage-day.nvim plugin

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -99,4 +99,15 @@ return {
       }
     end,
   },
+
+  {
+    "zeioth/garbage-day.nvim",
+    -- https://github.com/Zeioth/garbage-day.nvim
+    dependencies = "neovim/nvim-lspconfig",
+    event = "VeryLazy",
+    -- ../../../../../.local/share/nvim/lazy/garbage-day.nvim/lua/garbage-day/config.lua
+    opts = {
+      -- your options here
+    },
+  },
 }
