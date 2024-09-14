cd04d9b9430761eb1615fade5b5c60b93e1ff9cb
Mika Vilpas <mika.vilpas@gmail.com>
2024-09-14 18:15:56 +0300

    perf(nvim): lazy load cmp-rg on keypress

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -110,7 +110,6 @@ return {
   {
     -- https://github.com/lukas-reineke/cmp-rg
     "lukas-reineke/cmp-rg",
-    event = { "InsertEnter" },
     keys = {
       {
         -- https://github.com/lukas-reineke/cmp-rg/issues/46#issuecomment-1345672195
