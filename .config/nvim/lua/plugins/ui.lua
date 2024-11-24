d1ed88ba5197d1da1909ef04a58caa892dd5d123
Mika Vilpas <mika.vilpas@gmail.com>
2024-11-24 19:54:44 +0200

    feat(nvim): switch beacon to smear-cursor

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -23,8 +23,8 @@ return {
       },
     },
   },
-  -- Whenever cursor jumps some distance or moves between windows, it will
-  -- flash so you can see where it is
-  -- https://github.com/DanilaMihailov/beacon.nvim
-  "danilamihailov/beacon.nvim",
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {},
+  },
 }
