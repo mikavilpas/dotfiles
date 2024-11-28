bd45d22df83756836f922db38539fc10580e9272
Mika Vilpas <mika.vilpas@gmail.com>
2024-11-28 12:37:50 +0200

    feat(nvim): opt in to legacy_computing_symbols_support for smear-cursor

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -25,6 +25,11 @@ return {
   },
   {
     "sphamba/smear-cursor.nvim",
-    opts = {},
+    -- https://github.com/sphamba/smear-cursor.nvim
+    opts = {
+      -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+      -- Smears will blend better on all backgrounds.
+      legacy_computing_symbols_support = true,
+    },
   },
 }
