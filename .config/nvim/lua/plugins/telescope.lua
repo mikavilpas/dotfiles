b30cf80b73ce66d575ec74c2a82cf664671385db
Mika Vilpas <mika.vilpas@gmail.com>
2024-12-31 08:14:40 +0200

    refactor(nvim): remove telescope-fzf-native.nvim in favor of nucleo

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -110,22 +110,10 @@ return {
 
   {
     -- FZF sorter for telescope written in c
+    -- this gets enabled by LazyVim's telescope extra
     -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
     "nvim-telescope/telescope-fzf-native.nvim",
-    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          fzf = {
-            override_generic_sorter = true,
-            -- use natecraddock/telescope-zf-native.nvim for files
-            override_file_sorter = false,
-            case_mode = "smart_case",
-          },
-        },
-      })
-      require("telescope").load_extension("fzf")
-    end,
+    enabled = false,
   },
 
   {
