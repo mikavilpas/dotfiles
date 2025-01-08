679d82015e47b73dee4afc6c15406094dc67dd3b
Mika Vilpas <mika.vilpas@gmail.com>
2025-01-08 21:37:47 +0200

    feat(nvim): update blink and remove custom snippet config

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -24,30 +24,17 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     local my_opts = {
-      snippets = {
-        -- https://github.com/Saghen/blink.cmp?tab=readme-ov-file#luasnip
-        expand = function(snippet)
-          require("luasnip").lsp_expand(snippet)
-        end,
-        active = function(filter)
-          if filter and filter.direction then
-            return require("luasnip").jumpable(filter.direction)
-          end
-          return require("luasnip").in_snippet()
-        end,
-        jump = function(direction)
-          require("luasnip").jump(direction)
-        end,
-      },
       signature = {
         enabled = true,
+      },
+      snippets = {
+        preset = "luasnip",
       },
       sources = {
         default = {
           "lsp",
           "path",
-          -- "snippets",
-          "luasnip",
+          "snippets",
           "buffer",
           "ripgrep",
         },
