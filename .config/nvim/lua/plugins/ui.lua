a3d9559e52b5ab20fc539384c1e7b503b76d7fcf
Mika Vilpas <mika.vilpas@gmail.com>
2025-02-09 09:18:29 +0200

    refactor(nvim): update dressing.nvim URL to the main repo

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -7,7 +7,7 @@ return {
     -- core UI hooks (vim.ui.select and vim.ui.input). They exist to allow
     -- plugin authors to override them with improvements upon the default
     -- behavior, so that's exactly what we're going to do.
-    -- https://github.com/blob42/dressing.nvim
+    -- https://github.com/stevearc/dressing.nvim
     "stevearc/dressing.nvim",
     opts = { input = { insert_only = false } },
   },
