cd65b24993769762027a4ff05756d1aa1a2e288c
Mika Vilpas <mika.vilpas@gmail.com>
2025-02-10 11:24:29 +0200

    feat(nvim): use snacks.picker for yazi integrations

--- .config/nvim/lua/plugins/my-file-manager.lua
+++ .config/nvim/lua/plugins/my-file-manager.lua
@@ -22,9 +22,8 @@ return {
       },
       -- log_level = vim.log.levels.DEBUG,
       integrations = {
-        grep_in_directory = function(directory)
-          require("my-nvim-micro-plugins.main").my_live_grep({ cwd = directory })
-        end,
+        grep_in_directory = "snacks.picker",
+        grep_in_selected_files = "snacks.picker",
       },
       future_features = {
         process_events_live = true,
