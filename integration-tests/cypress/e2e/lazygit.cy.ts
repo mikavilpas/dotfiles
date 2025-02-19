94db269653fdbf38259ef154cd62fd12b899f41c
Mika Vilpas <mika.vilpas@gmail.com>
2025-02-19 22:14:57 +0200

    feat: add e2e testing setup for my personal config (#3)
    * feat: add e2e testing setup for my personal config
    
    Using https://github.com/mikavilpas/tui-sandbox
    
    * WIP: work in progress
    
    * WIP: work in progress
    
    * WIP: work in progress
    
    * WIP: work in progress
    
    * WIP: work in progress

---
+++ integration-tests/cypress/e2e/lazygit.cy.ts
@@ -0,0 +1,55 @@
+import assert from "assert"
+
+describe("lazygit", () => {
+  it("sanity check: .gitconfig and lazygit config are available for tests", () => {
+    cy.visit("/")
+    cy.startTerminalApplication({ commandToRun: ["bash"] }).then((t) => {
+      t.runBlockingShellCommand({ command: "echo $HOME" }).then((output) => {
+        assert(output.type === "success")
+        expect(output.stdout).includes("testdirs/")
+      })
+
+      t.runBlockingShellCommand({ command: "ls -al $HOME" }).then((output) => {
+        assert(output.type === "success")
+        expect(output.stdout).includes(".gitconfig")
+      })
+
+      t.runBlockingShellCommand({
+        command: "test -f ~/.config/lazygit/config.yml",
+      }).then((output) => {
+        assert(output.type === "success")
+      })
+
+      t.runBlockingShellCommand({
+        command: `git config --list --show-origin`,
+      }).then((output) => {
+        assert(output.type === "success")
+        expect(output.stdout).includes(".gitconfig")
+      })
+    })
+  })
+
+  it("can create a backup branch", () => {
+    cy.visit("/")
+    cy.startTerminalApplication({ commandToRun: ["bash"] })
+    cy.typeIntoTerminal("mkdir myrepo && cd myrepo{enter}")
+    cy.typeIntoTerminal("git init{enter}")
+    cy.typeIntoTerminal("touch README.md{enter}")
+    cy.typeIntoTerminal("git add .{enter}")
+    cy.typeIntoTerminal('git commit -m "initial commit"{enter}')
+    cy.typeIntoTerminal("lazygit{enter}")
+
+    cy.contains("Donate")
+
+    // enter the branch pane and wait for the branch to be selected
+    cy.typeIntoTerminal("3")
+    cy.contains("Author:")
+
+    // create a backup branch
+    cy.typeIntoTerminal("?")
+    cy.contains("Backup branch")
+    cy.typeIntoTerminal("b")
+
+    cy.contains("main--backup-")
+  })
+})
