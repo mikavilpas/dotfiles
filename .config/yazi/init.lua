ae335d03c474864f499d0c25623b3187341ee870
Mika Vilpas <mika.vilpas@gmail.com>
2025-03-13 10:55:00 +0200

    feat(yazi): reenable git plugin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -11,5 +11,24 @@ require("session"):setup({
 })
 
 -- https://github.com/yazi-rs/plugins/tree/main/git.yazi
--- ./plugins/git.yazi/
-require("git"):setup({})
+th.git = th.git or {}
+
+th.git.added = ui.Style():fg("blue")
+th.git.added_sign = ""
+
+th.git.deleted = ui.Style():fg("red"):bold()
+th.git.deleted_sign = ""
+
+th.git.ignored = ui.Style():fg("gray")
+th.git.ignored_sign = ""
+
+th.git.modified = ui.Style():fg("green")
+th.git.modified_sign = ""
+
+th.git.untracked = ui.Style():fg("gray")
+th.git.untracked_sign = ""
+
+th.git.updated = ui.Style():fg("green")
+th.git.updated_sign = ""
+
+require("git"):setup()
