359271a5d828b72814b4a74a05d31f7903a4b309
Mika Vilpas <mika.vilpas@gmail.com>
2025-05-11 16:05:15 +0300

    feat(nvim): colorize SnacksPickerPickWin as `peach`
    https://github.com/mikavilpas/yazi.nvim/pull/952

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -88,6 +88,7 @@ return {
             HopNextKey1 = { bg = colors.none, fg = colors.teal, style = { "bold" } },
             HopNextKey2 = { bg = colors.none, fg = colors.subtext0, style = { "italic" } },
             CopilotSuggestion = { fg = colors.surface2, bg = colors.none },
+            SnacksPickerPickWin = { bg = colors.peach, fg = "#000000" },
           }
         end,
       },
