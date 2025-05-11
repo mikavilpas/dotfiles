4e99cc0c933abd614bd362e2555630b528ebb0fe
Mika Vilpas <mika.vilpas@gmail.com>
2025-05-11 19:46:41 +0300

    feat(nvim): use shorter form of issue-solution PR template snippet
    **Issue**:
    - the issue template takes too much space in the commit message.
    - when concatenating many commit messages into a PR description, the
      commit messages look the same as the old issue-solution template.
    
    **Solution**:
    Use this shorter form of the issue-solution template.

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -5,25 +5,19 @@ return {
     "L3MON4D3/LuaSnip",
     config = function()
       local ls = require("luasnip")
-
       local s = ls.snippet
       local t = ls.text_node
-      local i = ls.insert_node
 
       ls.add_snippets("gitcommit", {
         s("issue", {
-          t("Issue"),
-          t({ "", "=====" }),
-          t({ "", "" }),
-          t({ "", "" }),
-          i(1),
-          t({ "", "" }),
-          t({ "", "Solution" }),
-          t({ "", "========" }),
-          t({ "", "" }),
-          t({ "", "" }),
-          i(2),
-          t({ "", "" }),
+          t({
+            "**Issue**:",
+            "",
+            "",
+            "**Solution**:",
+            "",
+            "",
+          }),
         }),
       })
     end,
