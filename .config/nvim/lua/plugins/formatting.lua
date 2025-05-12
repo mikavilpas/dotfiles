84c57423b941f13bbbff35b449e76ae1d89ed234
Mika Vilpas <mika.vilpas@gmail.com>
2025-05-12 20:57:58 +0300

    refactor: update mason.nvim to mason-org/mason.nvim

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -7,7 +7,7 @@
 ---@type LazySpec
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       table.insert(opts.ensure_installed, "prettierd")
     end,
