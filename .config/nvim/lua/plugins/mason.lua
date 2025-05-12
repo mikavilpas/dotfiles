2e242bead271c0d85cd81bab2302982028837bd2
Mika Vilpas <mika.vilpas@gmail.com>
2025-05-12 20:59:45 +0300

    refactor(nvim): don't manually pin mason versions
    This is now done in LazyVim instead

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -2,10 +2,9 @@
 ---@type LazySpec
 -- add any tools you want to have installed below
 return {
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim" },
   {
     "mason-org/mason.nvim",
-    version = "^1.0.0",
     opts = {
       ensure_installed = {
         -- lua
