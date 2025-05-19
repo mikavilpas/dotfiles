df0fc82ae71d17dd158a011826c4846f9d9b9bbc
Mika Vilpas <mika.vilpas@gmail.com>
2025-05-19 11:47:11 +0300

    feat(nvim): issue snippet enters insert_node

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -7,11 +7,13 @@ return {
       local ls = require("luasnip")
       local s = ls.snippet
       local t = ls.text_node
+      local i = ls.insert_node
 
       ls.add_snippets("gitcommit", {
         s("issue", {
+          t("**Issue:** "),
+          i(1),
           t({
-            "**Issue:**",
             "",
             "",
             "**Solution:**",
