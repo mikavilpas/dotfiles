cf531b74d02ef9eed693ff5b88a500a69ca47b20
Mika Vilpas <mika.vilpas@gmail.com>
2025-06-20 13:44:22 +0300

    feat(nvim): adjust smear-cursor settings

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -149,4 +149,15 @@ return {
       },
     },
   },
+
+  {
+    "smear-cursor.nvim",
+    -- https://github.com/sphamba/smear-cursor.nvim
+    opts = {
+      stiffness = 0.8,
+      trailing_stiffness = 0.5,
+      distance_stop_animating = 0.5,
+      time_interval = 20, -- ms
+    },
+  },
 }
