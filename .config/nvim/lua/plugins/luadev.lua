686e94de8a2eb4cf3dfbba7173955d2dead2ada8
Mika Vilpas <mika.vilpas@gmail.com>
2025-10-02 17:38:24 +0300

    feat(nvim): add wezterm types for luadev

--- .config/nvim/lua/plugins/luadev.lua
+++ .config/nvim/lua/plugins/luadev.lua
@@ -4,6 +4,7 @@ return {
   { "LuaCATS/luassert", name = "luassert-types", lazy = true },
   { "LuaCATS/busted", name = "busted-types", lazy = true },
   { "Bilal2453/luvit-meta", name = "luvit-types", lazy = true },
+  { "DrKJeff16/wezterm-types", name = "wezterm-types", lazy = true },
   {
     "folke/lazydev.nvim",
     opts = function(_, opts)
@@ -11,6 +12,7 @@ return {
         { path = "luassert-types/library", words = { "assert" } },
         { path = "luvit-types/library", words = { "vim%.uv" } },
         { path = "busted-types/library", words = { "describe" } },
+        { path = "wezterm-types", mods = { "wezterm" } },
         { path = vim.env.VIMRUNTIME, words = { "vim" } },
       })
     end,
