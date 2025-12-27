5b0f5d4769c681f92668943f712f3280d977ef17
Mika Vilpas <mika.vilpas@gmail.com>
2025-12-27 16:49:59 +0200

    feat(nvim): replace typescript-tools.nvim with tsgo
    It looks like code actions are missing but it's way faster. If it works
    fine for me, I can keep it. Otherwise I can revert it.
    
    - https://github.com/neovim/nvim-lspconfig/blob/master/lsp/tsgo.lua
    - https://github.com/microsoft/typescript-go

--- .config/nvim/lua/plugins/typescript.lua
+++ .config/nvim/lua/plugins/typescript.lua
@@ -7,6 +7,11 @@ return {
       ---@type table<string, vim.lsp.Config>
       servers = {
         oxlint = {}, -- enable
+        tsgo = {
+          on_attach = function(client, bufnr)
+            require("twoslash-queries").attach(client, bufnr)
+          end,
+        },
         eslint = {
           settings = {
             -- Fix https://github.com/un-ts/eslint-plugin-import-x starting
@@ -40,56 +45,25 @@ return {
   },
 
   {
-    "pmizio/typescript-tools.nvim",
-    ft = {
-      "typescript",
-      "javascript",
-      "typescriptreact",
-      "javascriptreact",
+    "marilari88/twoslash-queries.nvim",
+    -- Usage
+    -- Write a '//    ^?' placing the sign '^' under the variable to inspected
+    event = "LspAttach",
+    keys = {
+      { "<leader>at", "<cmd>TwoslashQueriesInspect<cr>", desc = "Show typescript type" },
     },
-    dependencies = {
-      {
-        "marilari88/twoslash-queries.nvim",
-        -- Usage
-        -- Write a '//    ^?' placing the sign '^' under the variable to inspected
-        event = "LspAttach",
-        keys = {
-          { "<leader>at", "<cmd>TwoslashQueriesInspect<cr>", desc = "Show typescript type" },
-        },
-        config = function(_, opts)
-          local palette = require("catppuccin.palettes.macchiato")
-          local darken = require("catppuccin.utils.colors").darken
-          vim.api.nvim_set_hl(0, "MikaTwoslashQueries", {
-            fg = palette.base,
-            bg = darken(palette.blue, 0.8),
-          })
-          opts.multi_line = true
-          opts.highlight = "MikaTwoslashQueries"
-          opts.is_enabled = true
+    config = function(_, opts)
+      local palette = require("catppuccin.palettes.macchiato")
+      local darken = require("catppuccin.utils.colors").darken
+      vim.api.nvim_set_hl(0, "MikaTwoslashQueries", {
+        fg = palette.base,
+        bg = darken(palette.blue, 0.8),
+      })
+      opts.multi_line = true
+      opts.highlight = "MikaTwoslashQueries"
+      opts.is_enabled = true
 
-          require("twoslash-queries").setup(opts)
-        end,
-      },
-    },
-
-    opts = {
-      on_attach = function(client, bufnr)
-        require("twoslash-queries").attach(client, bufnr)
-      end,
-
-      settings = {
-        expose_as_code_action = "all",
-
-        tsserver_file_preferences = {
-          includeInlayParameterNameHints = "all",
-          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-          includeInlayFunctionParameterTypeHints = true,
-          includeInlayVariableTypeHints = true,
-          includeInlayPropertyDeclarationTypeHints = true,
-          includeInlayFunctionLikeReturnTypeHints = true,
-          includeInlayEnumMemberValueHints = true,
-        },
-      },
-    },
+      require("twoslash-queries").setup(opts)
+    end,
   },
 }
