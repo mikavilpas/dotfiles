d25c11638f0c7d8f42a7ad99451d6853049c4e3e
Mika Vilpas <mika.vilpas@gmail.com>
2026-01-20 19:43:48 +0200

    refactor(fish): remove workaround for atuin + fish issue
    - https://github.com/atuinsh/atuin/issues/2803
    - https://github.com/atuinsh/atuin/issues/2940

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,10 +24,7 @@ if status is-interactive && test -z "$CI"
 
     # Commands to run in interactive sessions can go here
     #
-    # atuin has a fish related issue. Need to temporarily work around it.
-    # https://github.com/atuinsh/atuin/issues/2803, https://github.com/atuinsh/atuin/issues/2940
-    # atuin init fish | source
-    atuin init fish | sed "s/-k up/up/g" | source
+    atuin init fish | source
     starship init fish | source
     zoxide init fish | source
     fzf --fish | source
