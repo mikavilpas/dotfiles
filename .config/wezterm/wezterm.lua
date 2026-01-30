16d5d8ff72fd4b9971ed8bf5a7d7ddaef668f87f
Mika Vilpas <mika.vilpas@gmail.com>
2026-01-30 19:48:13 +0200

    refactor(wezterm): remove unused zen-mode integration

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -145,31 +145,6 @@ config.keys = {
 	},
 }
 
--- integration with nvim and zen-mode
--- https://github.com/folke/zen-mode.nvim?tab=readme-ov-file#wezterm
-wezterm.on("user-var-changed", function(window, pane, name, value)
-	local overrides = window:get_config_overrides() or {}
-	if name == "ZEN_MODE" then
-		local incremental = value:find("+")
-		local number_value = tonumber(value)
-		if incremental ~= nil then
-			while number_value > 0 do
-				window:perform_action(wezterm.action.IncreaseFontSize, pane)
-				number_value = number_value - 1
-			end
-			overrides.enable_tab_bar = false
-		elseif number_value < 0 then
-			window:perform_action(wezterm.action.ResetFontSize, pane)
-			overrides.font_size = nil
-			overrides.enable_tab_bar = true
-		else
-			overrides.font_size = number_value
-			overrides.enable_tab_bar = false
-		end
-	end
-	window:set_config_overrides(overrides)
-end)
-
 config.mouse_bindings = {
 	{
 		event = { Down = { streak = 3, button = "Left" } },
