37bd1d970edda5f4a82ba1b1630a419e20b4b2ea
Mika Vilpas <mika.vilpas@gmail.com>
2026-02-04 07:32:27 +0200

    chore(nvim): remove deprecated dressing.nvim plugin

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -28,16 +28,6 @@ local my_layout = {
 ---@type LazySpec
 return {
   {
-    -- With the release of Neovim 0.6 we were given the start of extensible
-    -- core UI hooks (vim.ui.select and vim.ui.input). They exist to allow
-    -- plugin authors to override them with improvements upon the default
-    -- behavior, so that's exactly what we're going to do.
-    -- https://github.com/stevearc/dressing.nvim
-    "stevearc/dressing.nvim",
-    opts = { input = { insert_only = false } },
-  },
-
-  {
     "https://github.com/saghen/blink.indent",
     --- @module 'blink.indent'
     --- @type blink.indent.Config
