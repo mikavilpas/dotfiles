16a62566c54cc6f9ae30115e704336f531deac0e
Mika Vilpas <mika.vilpas@gmail.com>
2026-02-20 08:37:04 +0200

    feat(fish): add catppuccin macchiato theme to `sk`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@
 set -g fish_greeting ''
 export XDG_CONFIG_HOME="$HOME/.config"
 export SKIM_DEFAULT_COMMAND="fd"
-export SKIM_DEFAULT_OPTIONS="--height=40% --reverse"
+export SKIM_DEFAULT_OPTIONS="--height=40% --reverse --color=fg:#cad3f5,bg:#24273a,matched:#363a4f,matched_bg:#f0c6c6,current:#cad3f5,current_bg:#494d64,current_match:#24273a,current_match_bg:#f4dbd6,spinner:#a6da95,info:#c6a0f6,prompt:#8aadf4,cursor:#ed8796,selected:#ee99a0,header:#8bd5ca,border:#6e738d"
 export SKIM_CTRL_T_COMMAND="fd"
 export SKIM_ALT_C_COMMAND="fd --type directory"
 
