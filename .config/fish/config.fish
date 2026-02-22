fce8874cf759b9d44270b4dd91da9baf188a754b
Mika Vilpas <mika.vilpas@gmail.com>
2026-02-22 10:48:49 +0200

    feat(fish): add nicer colors for fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@
 
 set -g fish_greeting ''
 export XDG_CONFIG_HOME="$HOME/.config"
+export FZF_DEFAULT_OPTS="--color=fg:#cad3f5,bg:#24273a,hl:#f5a97f:bold,fg+:#cad3f5,bg+:#494d64,hl+:#f5a97f:bold,spinner:#a6da95,info:#c6a0f6,prompt:#8aadf4,pointer:#ed8796,marker:#ee99a0,header:#8bd5ca,border:#6e738d"
 
 # https://typicode.github.io/husky/how-to.html#for-multiple-commands
 export HUSKY=0
