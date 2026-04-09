59b381fc8af16f226b8d42f40cdefa10a557f689
Mika Vilpas <mika.vilpas@gmail.com>
2026-04-09 07:27:42 +0300

    perf(nvim): lazy load debugprint.nvim and blink.indent

--- .config/nvim/lua/plugins/debugprint2.lua
+++ .config/nvim/lua/plugins/debugprint2.lua
@@ -4,6 +4,15 @@
 ---@type LazySpec
 return {
   "https://github.com/andrewferrier/debugprint.nvim",
+  keys = {
+    { "g?p", desc = "debugprint below" },
+    { "g?P", desc = "debugprint above" },
+    { "g?v", desc = "debugprint variable below", mode = { "n", "v" } },
+    { "g?V", desc = "debugprint variable above", mode = { "n", "v" } },
+    { "g?o", desc = "debugprint textobj below" },
+    { "g?O", desc = "debugprint textobj above" },
+    { "g?d", desc = "delete debug prints" },
+  },
   ---@type debugprint.GlobalOptions
   opts = {
     print_tag = "🤔 DEBUGPRINT",
