e63246115a27401d3c7bc6bba84b66df5fefb419
Mika Vilpas <mika.vilpas@gmail.com>
2026-08-23 09:22:55 +0300

    fix(yazi): adapt to git plugin changes, show all glyphs
    https://github.com/yazi-rs/plugins/pull/218
    
    Also move the configuration from lua to the theme.toml file

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -12,27 +12,6 @@ require("session"):setup({
   sync_yanked = true,
 })
 
--- https://github.com/yazi-rs/plugins/tree/main/git.yazi
-th.git = th.git or {}
-
-th.git.added = ui.Style():fg("blue")
-th.git.added_sign = ""
-
-th.git.deleted = ui.Style():fg("red"):bold()
-th.git.deleted_sign = ""
-
-th.git.ignored = ui.Style():fg("gray")
-th.git.ignored_sign = ""
-
-th.git.modified = ui.Style():fg("green")
-th.git.modified_sign = ""
-
-th.git.untracked = ui.Style():fg("gray")
-th.git.untracked_sign = ""
-
-th.git.updated = ui.Style():fg("green")
-th.git.updated_sign = ""
-
 require("git"):setup()
 
 if os.getenv("YAZI_NVIM_ID") ~= nil then
