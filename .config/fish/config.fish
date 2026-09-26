7f12e2ea77686c1cab8f09765e98216a7adf9dee
Mika Vilpas <mika.vilpas@gmail.com>
2026-09-26 08:58:09 +0300

    feat(fish): add `mi` abbr for mise install

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -70,6 +70,7 @@ if status is-interactive && test -z "$CI"
     abbr --add -- ... 'cd ../../'
     abbr --add -- top btm
     abbr --add -- parallel rust-parallel
+    abbr --add -- mi 'mise install'
 
     # Run a command with the secrets fnox resolves for the current directory,
     # e.g. `f npm ci` in a Barona repo. Only real binaries work: fnox execs the
